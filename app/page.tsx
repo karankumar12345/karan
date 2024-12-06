dfd4c56603f2cef5144ca6200134caad62910954
karan1_2 <karankumar2004122@gmail.com>
2024-12-06 15:32:53 +0530

    Removed Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,12 @@ import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 
-import { FloatingNavDemo } from "@/components/floatingnavbar";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-      <FloatingNavDemo/>
+   
         <Hero />
         <Grid />
         <RecentProjects />
